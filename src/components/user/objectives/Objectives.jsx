67e9dce843a5fefce7e658635b6da2b4dc76d38c
totiton1997 <totiton1997a@gmail.com>
2021-08-06 22:30:06 +0700

    cuongpq commit

---
+++ src/components/user/objectives/Objectives.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import index from './index.css'
+
+function Objectives(props) {
+    return (
+        <div className="jss104 jss2">
+            <div className="MuiBox-root jss110 jss105">
+                <div className="MuiBox-root jss111 jss106">
+                    <span className="MuiBox-root jss112 jss109">
+                        <img src="https://storage.googleapis.com/tripi-assets/mytour/icons/icon_support_247.svg" alt="logo_support_247" style={{ width: '52px', height: '52px' }} />
+                        <div className="MuiBox-root jss113 jss107">
+                        </div>
+                    </span>
+                    <span className="MuiTypography-root MuiTypography-subtitle2" style={{ marginTop: '24px', whiteSpace: 'nowrap' }}>Hỗ
+                        trợ khách hàng 24/7</span>
+                    <span className="MuiTypography-root jss108 MuiTypography-body1">Chat là có, gọi là nghe, không quản đêm hôm,
+                        ngày nghỉ và ngày lễ.</span>
+                </div>
+                <div className="MuiBox-root jss114 jss106"><span className="MuiBox-root jss115 jss109">
+                    <img src="https://storage.googleapis.com/tripi-assets/mytour/icons/icon_best_price.svg" alt="logo_best_price" style={{ width: '52px', height: '52px' }} />
+                    <div className="MuiBox-root jss116 jss107">
+                    </div>
+                </span>
+                    <span className="MuiTypography-root MuiTypography-subtitle2" style={{ marginTop: '24px', whiteSpace: 'nowrap' }}>Giá
+                        tốt sát ngày</span><span className="MuiTypography-root jss108 MuiTypography-body1">Cam kết giá tốt nhất
+                            khi đặt gần ngày cho chuyến đi của bạn.</span>
+                </div>
+                <div className="MuiBox-root jss117 jss106"><span className="MuiBox-root jss118 jss109"><img src="https://storage.googleapis.com/tripi-assets/mytour/icons/icon_payment.svg" alt="logo_payment" style={{ width: '52px', height: '52px' }} />
+                    <div className="MuiBox-root jss119 jss107" />
+                </span><span className="MuiTypography-root MuiTypography-subtitle2" style={{ marginTop: '24px', whiteSpace: 'nowrap' }}>Thanh toán dễ dàng, đa dạng</span><span className="MuiTypography-root jss108 MuiTypography-body1">Bao gồm thêm chuyển khoản ngân hàng và tiền
+                    mặt tại cửa hàng.</span></div>
+                <div className="MuiBox-root jss120 jss106"><span className="MuiBox-root jss121 jss109"><img src="https://storage.googleapis.com/tripi-assets/mytour/icons/icon_total_hotel.svg" alt="logo_total_hotel" style={{ width: '52px', height: '52px' }} />
+                    <div className="MuiBox-root jss122 jss107" />
+                </span><span className="MuiTypography-root MuiTypography-subtitle2" style={{ marginTop: '24px', whiteSpace: 'nowrap' }}>Hơn 8000+ khách sạn dọc Việt Nam</span><span className="MuiTypography-root jss108 MuiTypography-body1">Hàng nghìn khách sạn, đặc biệt là 4 sao và 5
+                    sao, cho phép bạn thoải mái lựa chọn, giá cạnh tranh, phong phú.</span></div>
+            </div>
+        </div>
+    );
+}
+
+export default Objectives;
